06e1d9cf1d6badb8532ea69a51c8f158ef13ad4a
Omid <aligoo.omid@gmail.com>
2022-05-13 12:24:57 +0430

    m

---
+++ src/main/java/com/java/ticket/model/Ticket.java
@@ -0,0 +1,25 @@
+package com.java.ticket.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Ticket {
+    
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    
+    private String context;
+}
