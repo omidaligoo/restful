94c33bad0a57b7bda6304e32a0c87652f6be29d2
mostafavi <mostafavi.somaye95@gmail.com>
2022-07-10 02:14:07 +0430

    swagger3

--- src/main/java/com/java/ticket/TicketApplication.java
+++ src/main/java/com/java/ticket/TicketApplication.java
@@ -2,18 +2,15 @@ package com.java.ticket;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 @Configuration
 @SpringBootApplication
 @EnableScheduling
-@EnableSwagger2
 public class TicketApplication {
 
 	public static void main(String[] args) {
